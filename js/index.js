6f9792e787be5c2a6a1d44a3ffd2c7c5e592dd3e
EfranaOD <efrana508@gmail.com>
2024-01-27 23:41:11 +0530

    update

---
+++ js/index.js
@@ -0,0 +1,4 @@
+let a, b;
+a = prompt("enter a value");
+b = prompt("enter b value");
+console.log("max of " + a + ", " + b + " is " +Math.max(a, b) + " and " + "min of " + a + ", " + b + " is " +Math.min(a, b))
